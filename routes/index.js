f68d66e5e3e0ab8ceba9970676a2a0584151e621
anjana784 <anjanasumith784@gmail.com>
2022-07-24 23:53:29 +0530

    fix download

--- routes/index.js
+++ routes/index.js
@@ -14,13 +14,9 @@ router.post('/post', (req, res) => {
     if(typeof req.body.model == 'string') {
         console.log('model was loded to the api');
         const modelString = req.body.model;
-        fs.writeFile('./model.obj', modelString, 'utf-8', () => {
-            console.log("object have been written");
-        });
+        fs.writeFileSync('./model.obj', modelString)
     }
-    res.download('./model.obj');
-    res.status(200);
-    res.send(`<h1>Data was received</h1>`);    
+    res.download('./model.obj');  
 });
 
 module.exports = router;
